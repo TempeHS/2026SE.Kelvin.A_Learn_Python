af8d06e546db051b9017cd6a6a741b65c2abd63e
Kelvin A <126206720+TheFileExplorer@users.noreply.github.com>
2025-02-20 08:25:55 +0000

    Twtter

---
+++ 2-Loops/problems2/twttr.py
@@ -0,0 +1,9 @@
+Sntnce = input("Enter a sentence: ")
+Vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+for letter in Vowels:
+    Sntnce = Sntnce.replace(letter, '')
+    
+print(Sntnce)
+    
+        
+
